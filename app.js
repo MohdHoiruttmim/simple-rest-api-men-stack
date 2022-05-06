201ff5a6ec3d62cb110bf3be1502450d1a4655fd
= <=>
2022-05-06 13:51:06 +0700

    connect to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/todolist",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+
+db.once("open", () => console.log("Connected to MongoDB"));
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => {
-    console.log(`server is running on http://localhost:5000`);
-})
+  console.log(`server is running on http://localhost:5000`);
+});
